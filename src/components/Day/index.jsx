f8188f836b5dcc1e73ed20708c840824edaba8e9
ElyaDzhy <elmira.freshcode@gmail.com>
2023-04-28 17:24:33 +0300

    Install classnames lib

--- src/components/Day/index.jsx
+++ src/components/Day/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import styles from './Day.module.css'
 
+
 function Day( props ) {
   const { isCurrentMonth, number } = props;
-  console.log(isCurrentMonth)
+  // const className=
   return (
     <td className={styles.day}>
      {number }
