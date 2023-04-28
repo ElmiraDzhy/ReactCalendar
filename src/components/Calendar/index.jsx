713178886be7753170655b4a0a4bd50cd70ba72e
ElyaDzhy <elmira.freshcode@gmail.com>
2023-04-28 13:32:54 +0300

    Create CalendarContext

--- src/components/Calendar/index.jsx
+++ src/components/Calendar/index.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
-import CalendarDay from './CalendarDay';
-import CalendarBody from './CalendarBody';
-import styles from './Calendar.module.css';
+import React from "react";
+import CalendarDay from "./CalendarDay";
+import CalendarBody from "./CalendarBody";
+import styles from "./Calendar.module.css";
+import {CalendarContext} from "./../../contexts/CalendarContext";
 
 class Calendar extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			date: new Date(),
+		};
+	}
+
   render() {
-    return (
-      <article className={styles.container}>
-        <CalendarDay/>
-        <CalendarBody/>
-      </article>
-    )
-  }
+    const { date } = this.state;
+		return (
+			<CalendarContext.Provider value={date}>
+				<article className={styles.container}>
+					<CalendarDay />
+					<CalendarBody />
+				</article>
+			</CalendarContext.Provider>
+		);
+	}
 }
 
 export default Calendar;
+
