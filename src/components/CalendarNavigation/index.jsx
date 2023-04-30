d718384402671f18fb4e31bd0726cfd38efa3b3c
ElyaDzhy <elmira.freshcode@gmail.com>
2023-04-30 05:33:16 +0300

    Fix bug in CalendarNavigation

--- src/components/CalendarNavigation/index.jsx
+++ src/components/CalendarNavigation/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./CalendarNavigation";
+import styles from "./CalendarNavigation.module.css";
 import { setDate, subMonths, addMonths, isThisMonth, format } from "date-fns";
 import { CalendarContext } from "../../contexts/CalendarContext";
 
@@ -9,34 +9,36 @@ function CalendarNavigation(props) {
 			{([date, changeCurrentDay]) => {
 				const month = format(date, "MMMM");
 				const year = format(date, "yyyy");
-				<div className={styles.wrapper}>
-					<button
-						onClick={() => {
-							let newDate = setDate(subMonths(date, 1), 1);
-							if (isThisMonth(newDate)) {
-								newDate = new Date();
-							}
+				return (
+					<div className={styles.wrapper}>
+						<button
+							onClick={() => {
+								let newDate = setDate(subMonths(date, 1), 1);
+								if (isThisMonth(newDate)) {
+									newDate = new Date();
+								}
 
-							changeCurrentDay(newDate);
-						}}
-						className={styles.arrow}>
-						{"<"}
-					</button>
-					<h1 className={styles.title}>
-						{month} {year}
-					</h1>
-					<button
-						onClick={() => {
-							let newDate = setDate(addMonths(date, 1), 1);
-							if (isThisMonth(newDate)) {
-								newDate = new Date();
-							}
-							changeCurrentDay(newDate);
-						}}
-						className={styles.arrow}>
-						{">"}
-					</button>
-				</div>;
+								changeCurrentDay(newDate);
+							}}
+							className={styles.arrow}>
+							{"<"}
+						</button>
+						<h1 className={styles.title}>
+							{month} {year}
+						</h1>
+						<button
+							onClick={() => {
+								let newDate = setDate(addMonths(date, 1), 1);
+								if (isThisMonth(newDate)) {
+									newDate = new Date();
+								}
+								changeCurrentDay(newDate);
+							}}
+							className={styles.arrow}>
+							{">"}
+						</button>
+					</div>
+				);
 			}}
 		</CalendarContext.Consumer>
 	);
